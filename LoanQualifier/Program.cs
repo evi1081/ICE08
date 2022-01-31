c92ea3c715f4fe71aca8b2fa832338bad5bb5a6a
einga <einga@MSI>
2022-01-31 12:33:19 -0500

    Add project files.

---
+++ LoanQualifier/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace LoanQualifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Edwin Inga
+              NMAD-180
+              Professor Berrios
+              Practicing Nested If Statements
+              */
+
+            //setting variables
+            int salary, jobYears;
+
+            //asking user to input their salary
+            Console.Write("Enter your annual salary: ");
+            salary = Convert.ToInt32(Console.ReadLine());
+
+            //making if statement in the case that user's job pays more than 40k
+            if (salary >= 40000)
+            {
+                //asks user another question after meeting the minimum salary requirement
+                Console.Write("\nHow many years do you have on the job? ");
+                jobYears = Convert.ToInt32(Console.ReadLine());
+
+                //telling user that their job experience and salary qualifies them for the loan
+                if (jobYears >= 2)
+                {
+                    Console.WriteLine("\nYou qualify for the loan");
+                }
+                // telling user that their salary is okay but they don't have enough job experience for a loan
+                else if (jobYears < 2)
+                {
+                    Console.WriteLine("\nMinimum years at job not met");
+                }
+            }
+            // if user does not meet minimum salary requirement of 40k, user is told they don't qualify for loan
+            else if (salary < 40000)
+            {
+                Console.WriteLine("\nMinimum salary requirements not met");
+            }
+            Console.WriteLine("\nProgram end...exiting");
+        }
+    }
+}
---
+++ WageCalculator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace WageCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Edwin Inga
+              NMAD-180
+              Professor Berrios
+              Practicing Nested If Statements
+              1/30/2021
+              */
+
+            //setting variables
+            int hoursWorked;
+            double hourlyRate, grossPay;
+            const int BASE_HOURS = 40;
+            const double OT_MULTIPLIER = 1.5;
+
+            // Asking user for their hourly rate
+            Console.Write("Please enter your hourly rate: ");
+            hourlyRate = Convert.ToDouble(Console.ReadLine());
+
+            // Asking user their amount of hours worked
+            Console.Write("Please enter the number of hours worked: ");
+            hoursWorked = Convert.ToInt32(Console.ReadLine());
+
+            // calculating whether or not user works more hours than expected based off of the base hours, and if they do the Overtime multiplier is added into the wage formula
+            if (hoursWorked > BASE_HOURS)
+            {
+                grossPay = ((hourlyRate * OT_MULTIPLIER) * hoursWorked);
+            }
+
+            // if user does not work overtime, normal formula is used to calculate wage
+            else
+            {
+                grossPay = hoursWorked * hourlyRate;
+            }
+
+            Console.WriteLine("Your Weekly Gross Pay: $" + grossPay);
+        }
+    }
+}
